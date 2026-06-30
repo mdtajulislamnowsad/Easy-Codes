e8f4fec63edf9466fcbf0c6f93a1d9510cb722c0
mdtajulislamnowsad <mdtajulislamnowsad@gmail.com>
2026-06-30 16:16:21 +0600

    Printing linked list

---
+++ easycode.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+
+    public:
+        int val;
+        Node *next;
+
+    Node(int val){
+        this->val = val;
+        this->next = NULL;
+
+    }
+};
+
+void insert(Node* head,int val){
+
+}
+
+int main() {
+    Node* head = new Node(10);
+    Node* a = new Node(20);
+    Node* b = new Node(30);
+
+    head->next = a;
+    a->next = b;
+
+    insert(head,100)
+    
+    return 0;
+}
