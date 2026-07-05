4b6ec83a88e816683b8a0196a02781209b651ec4
mdtajulislamnowsad <mdtajulislamnowsad@gmail.com>
2026-07-05 23:31:21 +0600

    Linked List Index Insertion

--- .cph/easy_code.cpp
+++ .cph/easy_code.cpp
@@ -1,73 +1,77 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Node{
-
-    public:
+class Node {
+public:
     int val;
     Node *next;
-
-    Node(int val){
-        this->val =val;
-        this->next=NULL;
+    Node(int val) {
+        this->val = val;
+        this->next = NULL;
     }
 };
 
-void insert(Node* &head,Node* &tail,int val){
-
-    Node* newnode=new Node(val);
-    if(head == NULL){
-        head = newnode;
-        tail = newnode;
-    }
-    else{
-        tail->next=newnode;
-        tail = newnode;
-    }
-    
-}
-
-void remove(Node* head) {
-    Node* newnode = head;
-    while (newnode != NULL) {
-        Node* tmp = newnode;
-        while (tmp->next != NULL) {
-            if (tmp->next->val == newnode->val) {
-                Node* deletenode = tmp->next;
-                tmp->next = tmp->next->next;
-                delete deletenode;
-            } else {
-                tmp = tmp->next;
-            }
-        }
-        newnode = newnode->next;
-    }
-}
-
 int main() {
-        
     Node* head = NULL;
     Node* tail = NULL;
+    int count = 0;
 
     int val;
-    while (true)
-    {
-        cin >> val;
-        if(val == -1){
-            break;
+    while (cin >> val && val != -1) {
+        Node* newnode = new Node(val);
+        if (head == NULL) {
+            head = newnode;
+            tail = newnode;
+        } else {
+            tail->next = newnode;
+            tail = newnode;
         }
-        insert(head,tail,val);
+        count++;
     }
 
-    remove(head);
+    int t;
+    cin >> t;
+    while (t--) {
+        int idx, val;
+        cin >> idx >> val;
 
-    Node* tmp = head;
-    while (tmp != NULL)
-    {
-        cout << tmp->val << " ";
-        tmp=tmp->next;
+        if (idx > count) {
+            cout << "Invalid" << endl;
+        } 
+        else if (idx == 0) {
+            Node* newnode = new Node(val);
+            newnode->next = head;
+            head = newnode;
+            count++;
+            
+            Node* tmp = head;
+            while (tmp != NULL) {
+                cout << tmp->val << " ";
+                tmp = tmp->next;
+            }
+            cout << endl;
+        } 
+        else {
+            Node* tmp = head;
+            for (int i = 1; i < idx; i++) {
+                tmp = tmp->next;
+            }
+            Node* newnode = new Node(val);
+            newnode->next = tmp->next;
+            tmp->next = newnode;
+            
+            if (newnode->next == NULL) tail = newnode;
+            
+            count++;
+            
+            Node* print = head;
+            while (print != NULL) {
+                cout << print->val << " ";
+                print = print->next;
+            }
+            cout << endl;
+        }
     }
-    
-   
+
     return 0;
 }
