6afa17914ed88882cb04cc6b4b70caba3a4c5616
mdtajulislamnowsad <mdtajulislamnowsad@gmail.com>
2026-07-06 22:56:09 +0600

    The Chain Builder

--- .cph/easy_code.cpp
+++ .cph/easy_code.cpp
@@ -1,77 +1,87 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Node {
-public:
+class Node{
+
+    public:
     int val;
     Node *next;
-    Node(int val) {
-        this->val = val;
-        this->next = NULL;
+
+    Node(int val){
+        this->val =val;
+        this->next=NULL;
     }
 };
 
+void insert(Node* &head,Node* &tail,int val){
+
+    Node* newnode=new Node(val);
+    if(head == NULL){
+        head = newnode;
+        tail = newnode;
+    }
+    else{
+        tail->next=newnode;
+        tail = newnode;
+    }
+    
+}
+
+
 int main() {
+        
     Node* head = NULL;
     Node* tail = NULL;
+
     int count = 0;
 
     int val;
-    while (cin >> val && val != -1) {
-        Node* newnode = new Node(val);
-        if (head == NULL) {
-            head = newnode;
-            tail = newnode;
-        } else {
-            tail->next = newnode;
-            tail = newnode;
+    while (true)
+    {
+        cin >> val;
+        if(val == -1){
+            break;
         }
+        insert(head,tail,val);
         count++;
     }
 
     int t;
     cin >> t;
-    while (t--) {
-        int idx, val;
+
+    while (t--)
+    {
+        int idx,val;
         cin >> idx >> val;
 
-        if (idx > count) {
-            cout << "Invalid" << endl;
-        } 
-        else if (idx == 0) {
-            Node* newnode = new Node(val);
-            newnode->next = head;
-            head = newnode;
-            count++;
-            
-            Node* tmp = head;
-            while (tmp != NULL) {
-                cout << tmp->val << " ";
-                tmp = tmp->next;
-            }
-            cout << endl;
-        } 
-        else {
-            Node* tmp = head;
-            for (int i = 1; i < idx; i++) {
-                tmp = tmp->next;
-            }
-            Node* newnode = new Node(val);
-            newnode->next = tmp->next;
-            tmp->next = newnode;
-            
-            if (newnode->next == NULL) tail = newnode;
-            
-            count++;
-            
-            Node* print = head;
-            while (print != NULL) {
-                cout << print->val << " ";
-                print = print->next;
-            }
-            cout << endl;
-        }
-    }
+        Node* newnode = new Node(val);
 
+        if(idx > count){
+            cout << "Invalid" << endl;
+        }
+        else{
+            Node* tmp = head;
+            for (int i = 1; i < idx; i++)
+            {
+                tmp = tmp->next;
+            }
+            newnode->next=tmp->next;
+            tmp->next= newnode;
+            count++;
+        }
+
+        Node* print=head;
+        while (print != NULL)
+        {
+            cout << print->val << " ";
+            print = print->next;
+        }
+        cout <<endl;
+    }
+    
+
+
+    
+   
     return 0;
 }
