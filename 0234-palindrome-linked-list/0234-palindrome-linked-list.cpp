69e767aee5186b7f2a92c31173a74df167095a8f
mdtajulislamnowsad <mdtajulislamnowsad@gmail.com>
2026-07-21 00:06:19 +0600

    Time: 109 ms (5.62%), Space: 157.4 MB (5.04%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+
+    void insert_at_tail(ListNode* &head,ListNode* &tail,int val){
+
+        ListNode* newnode = new ListNode (val);
+        if(head == NULL){
+            head = newnode;
+            tail = newnode;
+            return;
+        }
+        tail->next = newnode;
+        tail = newnode;
+    }
+
+    void reverse(ListNode* &head,ListNode* tmp){
+
+        if(tmp->next == NULL){
+            head = tmp;
+            return;
+        }
+        reverse(head,tmp->next);
+        tmp->next->next = tmp;
+        tmp->next = NULL;
+    }   
+
+    bool isPalindrome(ListNode* head) {
+
+        ListNode* newhead = NULL;
+        ListNode* newtail = NULL;
+
+        ListNode* tmp = head;
+        while(tmp != NULL){
+            insert_at_tail(newhead, newtail, tmp->val);
+            tmp= tmp->next;
+        }
+        
+        reverse(newhead,newhead);
+
+        tmp = head;
+        ListNode* tmp2 = newhead;
+        while(tmp != NULL){
+
+            if(tmp->val != tmp2->val){
+                return false;                
+            }
+
+            tmp = tmp->next;
+            tmp2 = tmp2->next;
+        }
+        return true;
+    }
+};
