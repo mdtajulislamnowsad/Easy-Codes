afce2429f8f8ed4eedf32e0f6b41a62bbf15e1b3
mdtajulislamnowsad <mdtajulislamnowsad@gmail.com>
2026-07-21 23:24:42 +0600

    Stack Input and Output with STL

---
+++ .cph/STL/STL_stack.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    stack<int> stk;
+
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        stk.push(x);
+    }
+
+    while (!stk.empty())
+    {
+        cout<<stk.top()<<" ";
+        stk.pop();
+    }
+    
+    return 0;
+}
