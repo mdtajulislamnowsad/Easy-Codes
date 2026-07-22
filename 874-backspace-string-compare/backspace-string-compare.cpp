4f0cd079b8cd8b9e36d664127002d4e6beb1594d
mdtajulislamnowsad <mdtajulislamnowsad@gmail.com>
2026-07-22 22:26:18 +0600

    Time: 0 ms (100.00%) | Memory: 8.5 MB (47.43%) - LeetSync

---
+++ 874-backspace-string-compare/backspace-string-compare.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+
+        stack<char> st_s;
+        stack<char> st_t;
+
+        for(char c: s){
+            if(c == '#'){
+                if(!st_s.empty())
+                    st_s.pop();
+            }
+            else
+                st_s.push(c);
+        }
+
+        for(char c: t){
+            if(c == '#'){
+                if(!st_t.empty())
+                    st_t.pop();
+            }
+            else
+                st_t.push(c);
+        }
+
+        return st_s == st_t;
+    }
+};
