5d3aa7fcf1ed68fcd65798888afbc226aaf437fd
mdtajulislamnowsad <mdtajulislamnowsad@gmail.com>
2026-07-24 23:34:48 +0600

    practice

--- Same_or_Not.cpp
+++ Same_or_Not.cpp
@@ -4,40 +4,41 @@ using namespace std;
 int main() {
     int s,q;
     cin >> s >> q;
-    stack<int>st;
-    queue<int>qt;
+    vector<int>st;
+    vector<int>qt;
 
     for (int i = 0; i < s; i++)
     {
         int x;
         cin >> x;
-        st.push(x);
+        st.push_back(x);
     }
     
     for (int i = 0; i < q; i++)
     {
         int x;
         cin >> x;
-        qt.push(x);
+        qt.push_back(x);
     }
+
+    
 
     if (s != q) {
         cout << "NO" << endl;
         return 0;
     }
-
     int triger = 1;
 
-
+    
     while(!st.empty() && !qt.empty()){
-        if(st.top() != qt.front()){
+        if(st.back() != qt.front()){
             triger = 0;
             break;
         }
 
         else{
-            st.pop();
-            qt.pop();
+            st.pop_back();
+            qt.erase(qt.begin());
         }
     }
 
